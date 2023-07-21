53f2f0d8129e0b6c42301045a857ea2970830abf
Nadi Duno <nadiduno@gmail.com>
2023-07-21 05:15:46 -0300

    API new

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
-const urlApi = "https://old-shrimp-28.loca.lt/zodic";
+const urlApi = "https://api.jsonbin.io/v3/b/64ba1bd4b89b1e2299c1dbc0";
 fetch(urlApi)
 fetch(urlApi)
     .then((response) => {
         return response.json();
     })
     .then((data) => {
-        // console.log(data)
-        let zodic = data[0]["avatar"]
-        document.getElementById("zodicName").innerHTML = data[0]["name"]
-        document.getElementById("zodicImg").setAttribute('src', zodic)
+        // Estudando o JSON
+        console.log("API dos Cavaleiros do Zodíaco: https://api.jsonbin.io/v3/b/64ba1bd4b89b1e2299c1dbc0")
+        console.log(data)
+
+        let nomeZodiaz = data["record"]["Zodic"]["0"]["name"]
+        let avatarZodiac = data["record"]["Zodic"]["0"]["avatar"]
+        document.getElementById("zodicName").innerHTML = nomeZodiaz
+        document.getElementById("zodicImg").setAttribute('src', avatarZodiac)
+
+
     })
     .catch((error) => {
         console.log(error)
