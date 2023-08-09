9c58fc0d69add052a3b420419b1a33d31ab9b131
Nadi Duno <nadiduno@gmail.com>
2023-08-09 16:54:18 -0300

    Refactoring o JavaScript

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
+
 async function userData() {
     await fetch("https://api.jsonbin.io/v3/b/64ba1bd4b89b1e2299c1dbc0")
         .then((response) => {
             return response.json();
         })
         .then((data) => {
-            // Estudando o JSON
+            // Estudando os dados 
             console.log(data)
+
+            let zodicName = document.createElement('p')
+            let zodicImg = document.createElement('img')
+
+            zodicName.textContent = data["record"]["Zodic"]["0"]["name"]
+            zodicImg.src = data["record"]["Zodic"]["0"]["avatar"]
+
+            root.append(zodicName, zodicImg)
         })
         .catch((error) => {
             console.error(error)
         })
 }
 userData()
-
-
