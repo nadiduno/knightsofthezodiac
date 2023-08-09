5a6b0876fa20cc4f7f7643f6a222cff3803a8ee5
Nadi Duno <nadiduno@gmail.com>
2023-08-09 16:32:42 -0300

    Add Async - await

--- main.js
+++ main.js
@@ -1,21 +1,16 @@
-const urlApi = "https://api.jsonbin.io/v3/b/64ba1bd4b89b1e2299c1dbc0";
-fetch(urlApi)
-fetch(urlApi)
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        // Estudando o JSON
-        console.log("API dos Cavaleiros do Zodíaco: https://api.jsonbin.io/v3/b/64ba1bd4b89b1e2299c1dbc0")
-        console.log(data)
+async function userData() {
+    await fetch("https://api.jsonbin.io/v3/b/64ba1bd4b89b1e2299c1dbc0")
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            // Estudando o JSON
+            console.log(data)
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+}
+userData()
 
-        let nomeZodiaz = data["record"]["Zodic"]["0"]["name"]
-        let avatarZodiac = data["record"]["Zodic"]["0"]["avatar"]
-        document.getElementById("zodicName").innerHTML = nomeZodiaz
-        document.getElementById("zodicImg").setAttribute('src', avatarZodiac)
 
-
-    })
-    .catch((error) => {
-        console.log(error)
-    })
